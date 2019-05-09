cde5b8ebca26fa2ca4cfa2efe0820d774714c32f
Mark Buchmoyer <mbuchmoyer@google.com>
2019-05-09 13:30:49 +0000

    add gameplay routes

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,13 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 
+import pprint
+
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello_handler():
   return 'hello world'
-
 
 @app.route('/')
 def home_handler():
@@ -15,5 +16,19 @@ def home_handler():
 
 @app.route('/test', methods = ['POST'])
 def post_test_handler():
+  pprint.pprint(request.data)
   return jsonify({'test':'output'})
+
+@app.route('/send_move', methods = ['POST'])
+def send_move():
+  return 'not implemented'
+
+@app.route('/get_graph', methods = ['GET'])
+def get_graph():
+  return 'not implemented'
+
+@app.route('/notification', methods = ['GET'])
+def notification():
+  return 'not implemented'
+
 
