fd5b67f38b12b643714766b950a54b251f262d2e
Andrew Emil <acemil@google.com>
2019-05-09 13:04:36 -0400

    Intermediate commit

--- ace/gameplay.js
+++ ace/gameplay.js
@@ -9,7 +9,7 @@ setInterval(function() {
 function eventLoopIteration() {
   console.log("starting event loop iteration");
   // Write move result
-  $.ajax({
+  $.post({
     url: "/send_move",
     data: {
       move: $('#move').val()
@@ -31,8 +31,26 @@ function eventLoopIteration() {
 }
 
 function handleGetData(result) {
+  var point_width = 5;
   console.log("handling get data result");
   // Update screen
+  var canvas = $('#canvas');
+  var context = canvas.getContext('2d');
+  context.strokeStyle = "blue";
+  context.lineWidth = 5;
+  var y_offset = 0;
+  for (stream in result.streams) {
+    var previous_y = stream[0];
+    for (var i = 1; i < stream.length() - 1; i++) {
+      var start_x = i * point_width;
+      var start_y = previous_y;
+      var width = point_width;
+      var height = stream[i];
+      cs.strokeRect(start_x, start_y, width, height + y_offset);
+      previous_y = start_y;
+    }
+    y_offset = y_offset + 1;
+  }
 }
 
 function handleWriteMove(result) {
